7d472e58eae05e073b14ea7b8ff0a7eb34c7f00f
Mehribon Alimova <alimovamehribon7@gmail.com>
2026-05-22 11:31:19 -0400

    Correct typos and logic in Bottle class
    Fix variable name typos and logic in updateAmount method.

--- question-2/Bottle.java
+++ question-2/Bottle.java
@@ -1,1 +1,23 @@
+public class Bottle{
+  private double amount;
+  private double initialAmount;
 
+  public Bottle(double amount){
+    this.amount = amount;
+    intialAmount = amount;
+  }
+
+  public double updateAmount(double amount){
+    double amount1 = intialAmount*0.25;
+    amount -= amountR;
+
+    if(amount < amount1){
+      amount = initialAmount;
+    }
+    return amount;
+
+  }
+
+}
+
+    
