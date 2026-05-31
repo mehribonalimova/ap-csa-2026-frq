ebb181573d8cdf68e1b6052dba3d932c7454eb90
Mehribon Alimova <alimovamehribon7@gmail.com>
2026-05-31 02:58:51 -0400

    Implement moreHistoryThanMathAbsences method
    Fix logic and syntax errors in moreHistoryThanMathAbsences method.

--- question-3/Attendance.java
+++ question-3/Attendance.java
@@ -1,1 +1,21 @@
+public int moreHistoryThanMathAbsences(){
+  int count = 0;
+  for(int i = 0; i <historyList.size(); i++)
+    {
+      CourseRecord s1 = historyList.get(i);
+      for(int j = 0; j<mathList.size(); j++)
+        {
+          CourseRecord s2 = mathList.get(j);
+          if(s1.getStudentID().equals(s2.getStudentID())
+             {
+               if(s1.getAbsences()>s2.getAbsences())
+               {
+                 count++;
+               }
+          }
+        }
+    }
+  return count;
 
+}
+   
