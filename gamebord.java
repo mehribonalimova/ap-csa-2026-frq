429d01b9da508f50e3f62252852fc2362073be2d
Mehribon Alimova <alimovamehribon7@gmail.com>
2026-05-31 03:08:53 -0400

    Update gamebord.java

--- gamebord.java
+++ gamebord.java
@@ -1,1 +1,18 @@
+public int getPointsForRow(int row){
+  int totalPoints = 0;
+  boolean sameColor = true;
+  String colorCheck = board[row][0].getColor();
 
+for(int col = 0; col < board[row].length; col++){
+  totalPoints += board[row][col].getPoints();
+  if(!board[row][col].getColor().equals(colorCheck)){
+    sameColor = false;
+  }
+}
+
+if(sameColor){
+  return totalPoints*2;
+}
+
+return totalPoints;
+}
